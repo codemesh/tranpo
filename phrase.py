1120b116ba51af06811e8aa0ee960d7d6c801348
fengyu <renantsec@163.com>
2012-06-14 00:09:07 -0400

    Add class for phrase

---
+++ phrase.py
@@ -0,0 +1,22 @@
+class Phrase :
+    def __init__(self) :
+        self.index = -1
+        self.end = -1
+        self.thread_prev = None
+        self.thread_next = None
+        self.syntax_parent = None
+        self.syntax_next = None
+        self.syntax_sibling = None
+        self.syntax_accord = None
+        self.subject = None
+        self.object = None
+        self.predicate = None
+        self.predicative = None
+        self.type = -1 # type of phrase
+        self.center = None # reference to center word
+        self.role = None # role as child of parent
+        self.score = 0
+        self.has_verb = false
+        self.has_predicate = false
+        
+    
